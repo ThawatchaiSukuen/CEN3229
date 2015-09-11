4caf7f4d1fd9601d3ce3d1b3a9aec503594bfa65
Thawatchai Sukuen <j.phrae56@gmail.com>
2015-09-11 10:24:38 +0700

    new HelloWrok File

---
+++ HelloWrok.java
@@ -0,0 +1,9 @@
+public class HelloWrok {
+	public static void main(String[] args) {
+		String A = "มีไรอยากจะบอก";
+		String B = "ขอยืมตังหน่อยดิ";
+		String C = " อิอิ ^^";
+
+		System.out.println(A + "\n" + B + "\n" + C);
+	}
+}
