7ce43af2be7fa7a748dd6a7425e6a72683bb387d
ThawatchaiSukuen <j.phrae56@gmail.com>
2015-09-11 10:19:53 +0700

    Create ็helloword.java

---
+++ ็helloword.java
@@ -0,0 +1,13 @@
+/**
+  First Java progam - Hello Wolrd
+  @version December, 2005
+  @author faa xumsai
+ */
+ 
+class  HelloWorld {
+	//Java launcher will call this method to
+   //display string "Hello World!"
+	public static void main(String[] args)  {
+		System.out.println("Hello World!");
+	}
+}
